e1bdd060fa7846ed0af9de744d1a0c2ae34d34a7
vboxuser <vboxuser@vbox.lan>
2025-05-23 13:21:04 +0200

    Fix GitHub Actions workflow to use SSH keys via secrets and terraform.tfvars

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -7,14 +7,33 @@ resource "aws_key_pair" "default" {
   public_key = var.public_key
 }
 
+resource "random_id" "suffix" {
+  byte_length = 2
+}
+
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+  name        = "allow_ssh_${random_id.suffix.hex}"
   description = "Allow SSH inbound traffic"
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "Port 3000 (e.g. frontend)"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Port 9090 (e.g. Prometheus)"
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
