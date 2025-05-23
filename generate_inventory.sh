614efa2d94d36d722c60361039cb49a2d395654e
vboxuser <vboxuser@vbox.lan>
2025-05-23 16:04:04 +0200

    fix(ansible): fix inventory generation and extract public IP from Terraform output

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ -s apply_output.txt ]]; then
+  echo -e "\n📄 Contenu de apply_output.txt :"
+  echo "--------------------------------"
+  cat apply_output.txt
+  echo "--------------------------------"
+else
+  echo "⚠️ Fichier apply_output.txt vide ou inexistant."
+fi
+
 IP=$(tail -n 1 terraform-aws/apply_output.txt | cut -d '"' -f2)
 
 # Vérification
