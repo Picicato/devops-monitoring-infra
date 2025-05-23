18442de07dd144a424f7d51f398a03f7cd62739b
vboxuser <vboxuser@vbox.lan>
2025-05-23 15:40:23 +0200

    Fix GitHub Actions workflow to use SSH keys via secrets and terraform.tfvars

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-IP=$(awk '/public_ip/ {gsub(/"/, "", $3); print $3}' terraform-aws/apply_output.txt)
+IP=$(awk '/public_ip/ {gsub(/["=]/, "", $NF); print $NF}' apply_output.txt)
 
 # Vérification
 if [[ -z "$IP" ]]; then
-  echo "❌ Impossible de récupérer l'adresse IP depuis apply_output.txt"
+  echo "Impossible de récupérer l'adresse IP depuis apply_output.txt"
   exit 1
 fi
-
-echo "✅ IP extraite : $IP"
 
 cat > ansible/inventory <<EOF
 [devops-instance]
 $IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
 EOF
 
-echo "✅ Inventory file generated with IP: $IP"
+echo "Inventory file generated with IP: $IP"
