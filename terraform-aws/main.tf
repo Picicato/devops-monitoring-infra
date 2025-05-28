01433ad6b3d41af47ae5d712987289afbf985115
vboxuser <vboxuser@vbox.lan>
2025-05-28 16:37:43 +0200

    Fix Grafana issues

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -28,13 +28,6 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description      = "Port 9090 (e.g. Prometheus)"
-    from_port        = 9090
-    to_port          = 9090
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
   egress {
     from_port   = 0
     to_port     = 0
